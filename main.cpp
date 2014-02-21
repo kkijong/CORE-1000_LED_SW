a97760c7607c53e4041a1ec597d3711e9c4cb697
bcostm <kijong82@gmail.com>
2014-02-21 09:36:12 +0000

    Put different delays for ON and OFF states + add comments

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ DigitalOut myled(LED1);
 
 int main() {
     while(1) {
-        myled = 1;
-        wait(0.2);
-        myled = 0;
-        wait(0.2);
+        myled = 1; // LED is ON
+        wait(0.2); // 200 ms
+        myled = 0; // LED is OFF
+        wait(1.0); // 1 sec
     }
 }
